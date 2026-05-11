b16f3feaa8214f6519dec598f8023c59df45978a
soldPrac <matramartel@gmail.com>
2026-05-11 17:40:46 +0900

    git 오류 수정

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,5 @@
-import { Routes, Route, Link } from 'react-router-dom';
+import { Routes, Route, Link, useLocation } from 'react-router-dom';
 import { useAuth } from './contexts/AuthContext';
-import HomePage from './pages/HomePage';
 import SignUpPage from './pages/user/SignUpPage';
 import LoginPage from './pages/user/LoginPage';
 import BoardListPage from './pages/board/BoardListPage';
@@ -31,17 +30,21 @@ import UserInformation from './pages/user/UserInformation'
 
 
 function App() {
-  const hideHealthMenu =
-    location.pathname === '/allRecord' ||
-    location.pathname === '/check/all' ||
-    location.pathname === '/check/meal' ||
-    location.pathname === '/check/weight' ||
-    location.pathname === '/check/blood-pressure' ||
-    location.pathname === '/check/blood-sugar' ||
-    location.pathname === '/check/exercise' ||
-    location.pathname === '/check/medicine' ||
-    location.pathname === '/AllRecordPage';
+  const { pathname } = useLocation();
 
+  const hideHealthMenu =
+    location.pathname === '/' ||
+    location.pathname === '/login' ||
+    location.pathname === '/signup' ||
+    location.pathname === '/boards' ||
+    location.pathname === '/posts/create' ||
+    location.pathname === '/mainreport' ||
+    location.pathname === '/osteoporosis' ||
+    location.pathname === '/diabetes' ||
+    location.pathname === '/gout' ||
+    location.pathname === '/hypertension' ||
+    location.pathname === '/dyslipidemia' ||
+    location.pathname === '/obesity' ||
     location.pathname.startsWith('/posts/');
 
   return (
